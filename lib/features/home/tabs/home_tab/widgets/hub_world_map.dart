ca91cdf4029486bf825bcb0b87ad4ad9d94fd752
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-23 20:47:54 +0300

    Updated WorldMap to display visa free countries

--- lib/features/home/tabs/home_tab/widgets/hub_world_map.dart
+++ lib/features/home/tabs/home_tab/widgets/hub_world_map.dart
@@ -1,20 +1,32 @@
 import 'package:countries_world_map/countries_world_map.dart';
 import 'package:countries_world_map/data/maps/world_map.dart';
 import 'package:flutter/material.dart';
+import 'package:passport_hub/common/models/country.dart';
 import 'package:passport_hub/common/models/visa_matrix.dart';
+import 'package:passport_hub/common/models/visa_requirement.dart';
 
 class HubWorldMap extends StatelessWidget {
   final VisaMatrix visaMatrix;
+  final Country selectedCountry;
 
   const HubWorldMap({
     super.key,
     required this.visaMatrix,
+    required this.selectedCountry,
   });
 
   @override
   Widget build(BuildContext context) {
-    final List<String> y = visaMatrix.countryList
-        .map((e) => e.iso2code)
+    final List<Country> visaFreeCountries =
+        visaMatrix.getCountriesByRequirement(
+      targetCountry: selectedCountry,
+      requirementType: VisaRequirementType.visaFree,
+    );
+
+    final List<String> y = visaFreeCountries
+        .map(
+          (Country e) => e.iso2code?.toLowerCase(),
+        )
         .whereType<String>()
         .toList();
     final Map<String, Color> x = Map<String, Color>.fromIterable(
@@ -22,7 +34,8 @@ class HubWorldMap extends StatelessWidget {
       value: (country) => Colors.green,
     );
     return InteractiveViewer(
-      maxScale: 75,
+      maxScale: 5,
+      scaleFactor: 1.0,
       child: SimpleMap(
         instructions: SMapWorld.instructions,
         defaultColor: Colors.grey,
