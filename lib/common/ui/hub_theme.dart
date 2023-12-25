760db29e17c2ec5668b1798d2c98d4e7bdffa1db
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-25 14:44:42 +0300

    UI Updates

--- lib/common/ui/hub_theme.dart
+++ lib/common/ui/hub_theme.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:passport_hub/common/models/visa_requirement.dart';
 
 class HubTheme {
-  static const Color primary = Color(0xff1E3A8A);
+  static const Color primary = Color(0xff001972);
   static const Color onPrimary = Color(0xff7DD3FC);
   static const Color secondary = Color(0xff10B981);
   static const Color onSecondary = Color(0xFF6EE7B7);
   static const Color background = Color(0xFFF3F4F6);
+  static const Color red = Color(0xffFF2525);
+  static const Color black = Color(0xff121212);
+  static const Color yellow = Colors.amber;
 
   static ThemeData get lightTheme {
     return ThemeData(
@@ -17,11 +21,11 @@ class HubTheme {
         background: background,
         secondary: secondary,
         onSecondary: onSecondary,
-        error: Color(0xFFB00020),
+        error: red,
         onError: Colors.white,
         onBackground: onPrimary,
         surface: Colors.white,
-        onSurface: Colors.black,
+        onSurface: black,
       ),
       scaffoldBackgroundColor: Colors.white,
       // Define button themes
@@ -37,4 +41,19 @@ class HubTheme {
   static const hubLargePadding = 24.0;
 
   static const hubBorderRadius = 8.0;
+}
+
+extension VisaRequirementTypeExtension on VisaRequirementType {
+  static Color get self => HubTheme.yellow;
+
+  Color get color {
+    return switch (this) {
+      VisaRequirementType.visaFree => HubTheme.primary,
+      VisaRequirementType.visaOnArrival => Colors.blue,
+      VisaRequirementType.eVisa => Colors.blue,
+      VisaRequirementType.visaRequired => HubTheme.red,
+      VisaRequirementType.noAdmission => HubTheme.red,
+      VisaRequirementType.none => HubTheme.black,
+    };
+  }
 }
