ba4ba75fea5d0eb61f545ad52a12da5684506c90
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-27 16:53:43 +0300

    Minor updates

--- lib/features/home/tabs/home_tab/explore_tab.dart
+++ lib/features/home/tabs/home_tab/explore_tab.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passport_hub/common/bloc/visa_bloc/visa_bloc.dart';
-import 'package:passport_hub/common/ui/hub_theme.dart';
 import 'package:passport_hub/common/ui/widgets/hub_page_title.dart';
-import 'package:passport_hub/features/home/tabs/home_tab/widgets/country_search_field.dart';
-import 'package:passport_hub/features/home/tabs/home_tab/widgets/country_search_results.dart';
 
 class ExploreTab extends StatelessWidget {
   const ExploreTab({super.key});
@@ -17,17 +14,6 @@ class ExploreTab extends StatelessWidget {
           return const Column(
             children: [
               HubPageTitle(title: "Explore"),
-              CountrySearchField(),
-              Expanded(
-                child: CustomScrollView(
-                  slivers: [
-                    SliverPadding(
-                      padding: EdgeInsets.only(top: HubTheme.hubMediumPadding),
-                      sliver: CountrySearchResults(),
-                    ),
-                  ],
-                ),
-              ),
             ],
           );
         },
