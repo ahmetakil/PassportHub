ff0293fbd7f127f867360a90d2222c690123db94
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-27 14:11:44 +0300

    Updates router for search

--- lib/common/app_router.dart
+++ lib/common/app_router.dart
@@ -3,12 +3,14 @@ import 'package:go_router/go_router.dart';
 import 'package:passport_hub/common/ui/hub_error_screen.dart';
 import 'package:passport_hub/features/country_details/country_details_screen.dart';
 import 'package:passport_hub/features/home/home_screen.dart';
+import 'package:passport_hub/features/search/search_screen.dart';
 import 'package:passport_hub/features/splash/screen/splash_screen.dart';
 
 class AppRouter {
   static const splash = "splash";
   static const home = "home";
   static const countryDetails = "countryDetails";
+  static const search = "search";
 
   static GoRouter generateGoRouter() {
     return GoRouter(
@@ -47,6 +49,12 @@ class AppRouter {
               isoCode: iso,
             );
           },
+        ),
+        GoRoute(
+          name: AppRouter.search,
+          path: "/$search",
+          builder: (BuildContext context, GoRouterState state) =>
+              const SearchScreen(),
         ),
       ],
     );
