1d789ffeb579b163b6a42e14e14354dcc0758809
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-29 00:33:08 +0300

    Implements HubCountryTile

---
+++ lib/common/ui/widgets/hub_country_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:passport_hub/common/models/country.dart';
+import 'package:passport_hub/common/ui/hub_theme.dart';
+import 'package:passport_hub/common/ui/widgets/hub_country_flag.dart';
+
+class HubCountryTile extends StatelessWidget {
+  final Country country;
+  final Widget? suffix;
+
+  const HubCountryTile({
+    super.key,
+    required this.country,
+    this.suffix,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(HubTheme.hubBorderRadius),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(
+          HubTheme.hubSmallPadding,
+        ),
+        child: Row(
+          children: [
+            HubCountryFlag(
+              country: country,
+            ),
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.only(left: HubTheme.hubSmallPadding),
+                child: Text(
+                  country.name ?? country.iso3code,
+                  maxLines: 2,
+                ),
+              ),
+            ),
+            const Spacer(),
+            if (suffix != null) suffix ?? const SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
