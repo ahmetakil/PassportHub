feef73604b96a1069339f3e805000d32d52669a4
Ahmet Akıl <ahmetakil.dev@gmail.com>
2023-12-29 01:54:04 +0300

    Re-adds the explore tab

--- lib/features/home/tabs/home_tab/explore_tab.dart
+++ lib/features/home/tabs/home_tab/explore_tab.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:passport_hub/common/app_router.dart';
+import 'package:passport_hub/common/bloc/country_search_bloc/country_search_bloc.dart';
 import 'package:passport_hub/common/bloc/visa_bloc/visa_bloc.dart';
+import 'package:passport_hub/common/models/country.dart';
+import 'package:passport_hub/common/ui/hub_theme.dart';
+import 'package:passport_hub/common/ui/widgets/hub_back_icon.dart';
+import 'package:passport_hub/common/ui/widgets/hub_country_tile.dart';
 import 'package:passport_hub/common/ui/widgets/hub_page_title.dart';
+import 'package:passport_hub/features/home/tabs/home_tab/widgets/country_search_field.dart';
 
 class ExploreTab extends StatelessWidget {
   const ExploreTab({super.key});
@@ -9,11 +17,56 @@ class ExploreTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: BlocBuilder<VisaBloc, VisaState>(
+      child: BlocBuilder<CountrySearchBloc, CountrySearchState>(
         builder: (context, state) {
-          return const Column(
+          final List<Country> allCountryList =
+              context.read<CountrySearchBloc>().allCountryList;
+          final List<Country> searchResults = state.getSearchResults();
+
+          final List<Country> countryList =
+              searchResults.isEmpty ? allCountryList : searchResults;
+
+          return Column(
             children: [
-              HubPageTitle(title: "Explore"),
+              const HubPageTitle(title: "Explore"),
+              const Padding(
+                padding: const EdgeInsets.only(
+                  bottom: HubTheme.hubMediumPadding,
+                ),
+                child: CountrySearchField(),
+              ),
+              Expanded(
+                child: CustomScrollView(
+                  slivers: [
+                    SliverList.separated(
+                      separatorBuilder: (_, __) => Divider(
+                        color: Colors.grey.withOpacity(0.2),
+                      ),
+                      itemCount: countryList.length,
+                      itemBuilder: (context, i) {
+                        final Country country = countryList[i];
+                        return InkWell(
+                          onTap: () {
+                            context.pushNamed(
+                              AppRouter.countryDetails,
+                              pathParameters: {
+                                "iso": country.iso3code,
+                              },
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: HubTheme.hubSmallPadding),
+                            child: HubCountryTile(
+                              country: country,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
           );
         },
