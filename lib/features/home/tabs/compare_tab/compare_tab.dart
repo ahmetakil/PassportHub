282be80951859d89ac14a3af7dcfecc884bda8cf
Ahmet Akıl <ahmetakil.dev@gmail.com>
2024-01-11 17:40:21 +0300

    Update default compare screen

--- lib/features/home/tabs/compare_tab/compare_tab.dart
+++ lib/features/home/tabs/compare_tab/compare_tab.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:passport_hub/common/ui/hub_theme.dart';
+import 'package:passport_hub/common/ui/widgets/hub_icon.dart';
+import 'package:passport_hub/common/ui/widgets/hub_page_title.dart';
 
 class CompareTab extends StatelessWidget {
   const CompareTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text(
-            "Coming soon",
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const HubPageTitle(title: "Compare"),
+          Spacer(),
+          const HubImage(
+            image: HubImages.comparePreview,
+            width: 240,
           ),
-        ),
-      ],
+          Text(
+            "COMING SOON",
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(
+              HubTheme.hubMediumPadding,
+            ),
+            child: Text(
+              'Compare and find out which passport gives you the most freedom to travel around the world!',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+          Spacer(
+            flex: 2,
+          ),
+        ],
+      ),
     );
   }
 }
