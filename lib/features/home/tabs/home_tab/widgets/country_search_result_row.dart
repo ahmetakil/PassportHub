33eddbdb6c85e2877e9b4892fdda23d93e6757f5
Ahmet Akıl <ahmetakil.dev@gmail.com>
2024-02-21 16:56:13 +0300

    Fixes compare not allowing deselection

--- lib/features/home/tabs/home_tab/widgets/country_search_result_row.dart
+++ lib/features/home/tabs/home_tab/widgets/country_search_result_row.dart
@@ -4,6 +4,7 @@ import 'package:passport_hub/common/bloc/country_search_bloc/country_search_bloc
 import 'package:passport_hub/common/models/country.dart';
 import 'package:passport_hub/common/ui/hub_theme.dart';
 import 'package:passport_hub/common/ui/widgets/hub_country_tile.dart';
+import 'package:passport_hub/common/ui/widgets/hub_snackbar.dart';
 import 'package:passport_hub/features/home/tabs/compare_tab/bloc/compare_bloc.dart';
 
 class CountrySearchResultTile extends StatelessWidget {
@@ -22,7 +23,24 @@ class CountrySearchResultTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        final CompareBloc compareBloc = context.read<CompareBloc>();
+
         if (isCompare) {
+          final bool selectedMaxAlready =
+              compareBloc.state.selectedCountryCount > 1;
+
+          /// To allow deselection.
+          final bool isCurrentlySelected =
+              compareBloc.state.isSelected(country);
+
+          if (selectedMaxAlready && !isCurrentlySelected) {
+            HubSnackbar.show(
+              context: context,
+              child: const Text("You can only select 2 countries to compare"),
+            );
+            return;
+          }
+
           context.read<CompareBloc>().add(
                 SelectCountryForCompareEvent(country: country),
               );
